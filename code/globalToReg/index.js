5aa822dd137a53f99650f0d6c9d3d2f6ce453c05
chentao.arthur <chentao.arthur@bytedance.com>
2021-11-19 20:18:01 +0800

    feat: add code

--- code/globalToReg/index.js
+++ code/globalToReg/index.js
@@ -1,4 +1,4 @@
 const globToRegExp = require('glob-to-regexp');
 
-const re = globToRegExp('*dist*');
+const re = globToRegExp('**/node_modules');
 console.log(re);
---
+++ code/walk/index.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const walk = require('walk');
+
+function start() {
+  let hasError;
+  const data = [];
+  const rootDir = __dirname;
+  const prefix = path.dirname(rootDir);
+
+  walk.walkSync(rootDir, {
+    listeners: {
+      file(root, {name}, next) {
+        if (path.extname(name) === '.css') {
+          data.push(`${root.replace(prefix, '')}/${name}`);
+        }
+        next();
+      },
+      errors(root, nodeStatsArray, next) {
+        hasError = true;
+        nodeStatsArray.forEach(({error}) => {
+          console.log(error);
+        })
+        next()
+      },
+    },
+  })
+  return hasError || data;
+}
+
+console.log(start());
