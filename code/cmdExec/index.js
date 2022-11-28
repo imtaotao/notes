6c23046a7d1475b65ee6737374be822b5ad155a0
chentao.arthur <chentao.arthur@bytedance.com>
2022-11-28 17:04:42 +0800

    feat: add code

---
+++ code/cmdExec/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+const cmdShim1 = require('cmd-shim');
+const cmdShim2 = require('@zkochan/cmd-shim');
+
+;(async () => {
+  const pkg = {
+    bin: {
+      test: './test.js',
+    }
+  }
+  const binName = 'test';
+  const thisDest = __dirname;
+  const binDest = `${thisDest}/.bin`;
+  const src = path.resolve(thisDest, pkg.bin[binName]);
+  
+  if (fs.existsSync(src)) {
+    fs.chmodSync(src, 0o755);
+  }
+  if (!fs.existsSync(binDest)) {
+    fs.mkdirSync(binDest, { recursive: true });
+  }
+
+  // await cmdShim2.ifExists(src, `${binDest}/${binName}`, { createPwshFile: false });
+  await cmdShim1.ifExists(src, `${binDest}/${binName}`, { createPwshFile: false });
+})();
