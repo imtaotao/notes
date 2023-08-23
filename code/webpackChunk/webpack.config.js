a477be758db0f14bdca30459942da6abc1017e9b
chentao.arthur <chentao.arthur@bytedance.com>
2023-08-23 15:18:47 +0800

    feat: add code

--- code/webpackChunk/webpack.config.js
+++ code/webpackChunk/webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
     },
   },
   optimization: {
-    minimize: true,
+    minimize: false,
     minimizer: [new TerserPlugin({ extractComments: false })],
     realContentHash: true,
     runtimeChunk: {
